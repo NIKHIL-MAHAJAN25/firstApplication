4534a739db671381a9d7913bf0e4396562cf1b8e
Nikhil mahajan <nikhilmahajan8787@gmail.com>
2025-02-28 15:55:49 +0530

    used multiple buttons

--- app/src/main/java/com/nikhil/firstapplication/MainActivity.kt
+++ app/src/main/java/com/nikhil/firstapplication/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.nikhil.firstapplication
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,6 +12,8 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         var button: Button?=null
+        var button2:Button?=null
+        var button3:Button?=null
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
@@ -19,5 +23,19 @@ class MainActivity : AppCompatActivity() {
             insets
         }
         button=findViewById(R.id.btnsignup)
+        button?.setOnClickListener()
+        {
+            var intent=Intent(this,RelativeActivity::class.java)
+            startActivity(intent)
+            Toast.makeText(this,"My button is clicked",Toast.LENGTH_SHORT).show()
+        }
+        button2=findViewById(R.id.btngoogle)
+        button2.setOnClickListener()
+        {
+            var intent1=Intent(this,google::class.java)
+            startActivity(intent1)
+            Toast.makeText(this,"google button is clicked",Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
